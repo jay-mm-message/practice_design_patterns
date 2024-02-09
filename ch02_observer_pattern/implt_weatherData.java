a3fff1fe3820876f0f31231eec1fa6514dd2cf42
jay <jay.mm.message@gmail.com>
2024-02-09 19:28:57 +0800

    add implement interface of subject

---
+++ ch02_observer_pattern/implt_weatherData.java
@@ -0,0 +1,32 @@
+package ch02_observer_pattern;
+
+public class implt_weatherData implements itrf_subject {
+
+    @Override
+    public void registerObservers(itrf_observer ob) {
+    }
+
+    @Override
+    public void removeObservers(itrf_observer ob) {
+    }
+
+    @Override
+    public void notifyObservers() {
+    }
+    
+    public void getTemperature() {
+
+    }
+
+    public void getHumidity() {
+
+    }
+
+    public void getPressure() {
+
+    }
+
+    public void measurementsChanged() {
+        
+    }
+}
