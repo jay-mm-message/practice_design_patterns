b7f372a8c3fe7b18dd0805021011ecb315538952
jay <jay.mm.message@gmail.com>
2024-02-09 19:54:42 +0800

    prepare subject register and remove

--- ch02_observer_pattern/implt_weatherData.java
+++ ch02_observer_pattern/implt_weatherData.java
@@ -1,17 +1,29 @@
 package ch02_observer_pattern;
 
-public class implt_weatherData implements itrf_subject {
+import java.util.ArrayList;
+import java.util.List;
 
+public class implt_weatherData implements itrf_subject {
+    private List<itrf_observer> observers;
+    
+    public implt_weatherData() {
+        observers = new ArrayList<itrf_observer>();
+    }
     @Override
-    public void registerObservers(itrf_observer ob) {
+    public void registerObservers(itrf_observer obsr) {
+        observers.add(obsr);
     }
 
     @Override
-    public void removeObservers(itrf_observer ob) {
+    public void removeObservers(itrf_observer obsr) {
+        observers.remove(obsr);
     }
 
     @Override
     public void notifyObservers() {
+        for (itrf_observer itrf_observer : observers) {
+            itrf_observer.update();
+        }
     }
     
     public void getTemperature() {
