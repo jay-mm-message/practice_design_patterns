5b420799df44abeceb336f074ed414b1c8a85ac9
jay <jay.mm.message@gmail.com>
2024-02-09 19:17:04 +0800

    add has-a behavior

--- ch02_observer_pattern/itrf_subject.java
+++ ch02_observer_pattern/itrf_subject.java
@@ -1,7 +1,7 @@
 package ch02_observer_pattern;
 
 public interface itrf_subject {
-    public void registerObservers();
-    public void removeObservers();
+    public void registerObservers(itrf_observer ob);
+    public void removeObservers(itrf_observer ob);
     public void notifyObservers();
 }
