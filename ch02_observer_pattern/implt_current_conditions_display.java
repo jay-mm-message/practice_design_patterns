37ffdbd91ca86117b0fbcba52ffae461106ff219
jay <jay.mm.message@gmail.com>
2024-02-09 19:25:15 +0800

    add three implement interface of observer

---
+++ ch02_observer_pattern/implt_current_conditions_display.java
@@ -0,0 +1,13 @@
+package ch02_observer_pattern;
+
+public class implt_current_conditions_display implements itrf_observer, itrf_display_element{
+
+    @Override
+    public void display() {
+    }
+
+    @Override
+    public void update() {
+    }
+    
+}
