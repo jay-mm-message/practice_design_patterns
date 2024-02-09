6f39b7dbd6374e8d7cc61f4b818b2168be009b5b
jay <jay.mm.message@gmail.com>
2024-02-09 19:37:50 +0800

    add has-a subject into three implement of observers

--- ch02_observer_pattern/implt_statistics_display.java
+++ ch02_observer_pattern/implt_statistics_display.java
@@ -2,6 +2,12 @@ package ch02_observer_pattern;
 
 public class implt_statistics_display implements itrf_observer, itrf_display_element {
 
+    public implt_weatherData weatherData;
+    
+    public implt_statistics_display(implt_weatherData weatherData) {
+        this.weatherData = weatherData;
+    }
+
     @Override
     public void display() {
     }
--- ch02_observer_pattern/implt_third_party_display.java
+++ ch02_observer_pattern/implt_third_party_display.java
@@ -2,6 +2,12 @@ package ch02_observer_pattern;
 
 public class implt_third_party_display implements itrf_observer, itrf_display_element{
 
+    public implt_weatherData weatherData;
+
+    public implt_third_party_display(implt_weatherData weatherData) {
+        this.weatherData = weatherData;
+    }
+
     @Override
     public void display() {
     }
